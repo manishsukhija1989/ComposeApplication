74f8d9ac3449be4d430712ff9ed19fb017345d13
manishsukhija1989 <manishsukhija1989@gmail.com>
2025-08-26 21:36:16 +0530

    Auto create build

--- app/src/main/java/com/manish/composeapplication/ui/activity/MainActivity.kt
+++ app/src/main/java/com/manish/composeapplication/ui/activity/MainActivity.kt
@@ -37,7 +37,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    println("This is for Automatic build check.")
+    println("This is for Automatic build check. Thank you")
     ComposeApplicationTheme {
 
     }
